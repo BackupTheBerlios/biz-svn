30d4c077f59fc6e8242ca56b3c64abc3f0c29ca4
yuce <yuce@c081ea2c-b811-0410-9846-ce044fc1b7b4>
2006-04-24 17:01:56 +0000

    favicon update

--- branches/components-branch/apps/favicon.py
+++ branches/components-branch/apps/favicon.py
@@ -5,16 +5,14 @@ from biz.content import FileContent, CachedFileContent, EmptyContent
 
 
 class Favicon(Application):
-	"""environ["biz.app.favicon.location"] should point the icon
-	(including the filename),
-	environ["biz.app.favicon.default"] may point to the fallback icon.
+	"""self.options["favicon.location"] should point to the icon
 	"""
 
 	def static(self):
-		for loc in ["favicon.location", "favicon.default"]:
-			if self.options.has_key(loc):
-				self.content = CachedFileContent(self.options[loc], "image/x-icon")
-				break
+		locopt = "favicon.location"
+		if self.options.has_key(locopt):
+			self.content = CachedFileContent(self.options[locopt], "image/x-icon")
+			break
 		else:
 			self.content = EmptyContent()
 
