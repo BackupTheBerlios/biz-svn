d222347d744fc769afa176429e27d89a9b6558cb
yuce <yuce@c081ea2c-b811-0410-9846-ce044fc1b7b4>
2006-04-30 19:14:48 +0000

    biz version set to 0.0.1

--- branches/xenviron-branch/__init__.py
+++ branches/xenviron-branch/__init__.py
@@ -5,6 +5,8 @@ from biz.app import Application, SecureApplication
 from biz.content import EmptyContent, TextContent, HtmlContent, \
 		FileContent, CachedFileContent
 
+__version__ = "0.0.1"
+
 __all__ = ["Root", "Application", "SecureApplication",
 			"EmptyContent", "TextContent", "HtmlContent",
 			"FileContent", "CachedFileContent"]
